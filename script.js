e305205c001cc16666d1cf41ddf6fecb4d25692f
monke <mack@monke.group>
2024-10-12 23:41:01 +0000

    better update

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const themeToggle = document.getElementById('theme-toggle'); // Target the toggle button
+    const sections = document.querySelectorAll("section");
 
-    // Check if a theme is already set in localStorage
-    const currentTheme = localStorage.getItem('theme');
-    if (currentTheme == 'light') {
-        document.body.classList.add('light-mode'); // Apply light mode if set
-    }
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    });
 
-    // Event listener for toggle button click
-    themeToggle.addEventListener('click', function() {
-        document.body.classList.toggle('light-mode'); // Toggle the light mode class
-
-        // Update the theme in localStorage
-        let theme = 'dark'; // Default to dark
-        if (document.body.classList.contains('light-mode')) {
-            theme = 'light'; // Set to light if the class is present
-        }
-        localStorage.setItem('theme', theme); // Save preference in localStorage
+    sections.forEach(section => {
+        observer.observe(section);
     });
 });
-
